30ff6693ce2e7cbe6cb4cf78d52638c38de96b5f
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-12 21:58:34 +0530

    add scroll

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -6,16 +6,19 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final CategoriesScroller categoriesScroller = CategoriesScroller();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
+      // backgroundColor: Color.fromRGBO(46, 64, 83, 1.0),
       appBar: AppBar(
         elevation: 0,
         title: Text(
           'Dasa Production',
           style: TextStyle(
             color: Colors.black,
+            letterSpacing: 1,
           ),
         ),
         centerTitle: true,
@@ -36,6 +39,46 @@ class _HomeState extends State<Home> {
               onPressed: () => {}),
         ],
       ),
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    'Cart',
+                    style: TextStyle(
+                      letterSpacing: 2,
+                      fontSize: 15.0,
+                    ),
+                  ),
+                  Text(
+                    'Videos',
+                    style: TextStyle(
+                      letterSpacing: 2,
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
+        ),
+      ),
     ));
+  }
+}
+
+class CategoriesScroller extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+    );
   }
 }
