02e4c8f22a2a9dfb032ecf29e02b51104ef04abb
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-14 10:25:46 +0530

    added new widgets

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -258,7 +258,7 @@ class CategoriesScroller extends StatelessWidget {
                     height: categoryHeight,
                     decoration: BoxDecoration(
                       image: DecorationImage(
-                        image: new AssetImage('assets/sanju.jpg'),
+                        image: new AssetImage('assets/images/harshana.jpg'),
                         fit: BoxFit.cover,
                       ),
                       color: Colors.orange.shade400,
@@ -279,7 +279,7 @@ class CategoriesScroller extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: <Widget>[
                           Text(
-                            'Sachintha Dilshan',
+                            'Harshana madushanka',
                             style: TextStyle(
                               fontWeight: FontWeight.bold,
                               color: Colors.white,
@@ -288,8 +288,8 @@ class CategoriesScroller extends StatelessWidget {
                               shadows: <Shadow>[
                                 Shadow(
                                   color: Colors.black,
-                                  offset: Offset(2, 2),
-                                  blurRadius: 10,
+                                  offset: Offset(4, 4),
+                                  blurRadius: 20,
                                 )
                               ],
                             ),
@@ -298,7 +298,7 @@ class CategoriesScroller extends StatelessWidget {
                             height: 5,
                           ),
                           Text(
-                            'Actor',
+                            'Director',
                             style: TextStyle(
                               fontSize: 15.0,
                               color: Colors.white,
@@ -306,8 +306,8 @@ class CategoriesScroller extends StatelessWidget {
                               shadows: <Shadow>[
                                 Shadow(
                                   color: Colors.black,
-                                  offset: Offset(1, 1),
-                                  blurRadius: 5,
+                                  offset: Offset(3, 3),
+                                  blurRadius: 15,
                                 )
                               ],
                             ),
@@ -325,7 +325,7 @@ class CategoriesScroller extends StatelessWidget {
                     height: categoryHeight,
                     decoration: BoxDecoration(
                       image: DecorationImage(
-                        image: new AssetImage('assets/shan.jpg'),
+                        image: new AssetImage('assets/sanju.jpg'),
                         fit: BoxFit.cover,
                       ),
                       color: Colors.orange.shade400,
@@ -349,7 +349,7 @@ class CategoriesScroller extends StatelessWidget {
                           mainAxisAlignment: MainAxisAlignment.end,
                           children: <Widget>[
                             Text(
-                              'Harshana Madushanka',
+                              'Sachintha Dilshan',
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
                                 color: Colors.white,
@@ -396,7 +396,7 @@ class CategoriesScroller extends StatelessWidget {
                     margin: EdgeInsets.only(right: 5),
                     decoration: BoxDecoration(
                       image: DecorationImage(
-                        image: new AssetImage('assets/shan.jpg'),
+                        image: new AssetImage('assets/images/akila.jpg'),
                         fit: BoxFit.cover,
                       ),
                       color: Colors.orange.shade400,
@@ -485,6 +485,207 @@ class CategoriesScroller extends StatelessWidget {
                         children: <Widget>[
                           Text(
                             'Pramuditha Pasanga',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 30.0,
+                              fontFamily: 'AmaticSC',
+                              shadows: <Shadow>[
+                                Shadow(
+                                  color: Colors.black,
+                                  offset: Offset(2, 2),
+                                  blurRadius: 10,
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            'Actor',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.white,
+                              fontFamily: 'Nunito',
+                              shadows: <Shadow>[
+                                Shadow(
+                                  color: Colors.black,
+                                  offset: Offset(1, 1),
+                                  blurRadius: 5,
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () => {},
+                  child: Container(
+                    width: 150,
+                    height: categoryHeight,
+                    margin: EdgeInsets.only(right: 5),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: new AssetImage('assets/images/tharindu.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                      color: Colors.orange.shade400,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          offset: Offset(2, 2),
+                          blurRadius: 2,
+                          spreadRadius: 0,
+                        )
+                      ],
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            'Tharindu Eranda',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 30.0,
+                              fontFamily: 'AmaticSC',
+                              shadows: <Shadow>[
+                                Shadow(
+                                  color: Colors.black,
+                                  offset: Offset(2, 2),
+                                  blurRadius: 10,
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            'Actor',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.white,
+                              fontFamily: 'Nunito',
+                              shadows: <Shadow>[
+                                Shadow(
+                                  color: Colors.black,
+                                  offset: Offset(1, 1),
+                                  blurRadius: 5,
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () => {},
+                  child: Container(
+                    width: 150,
+                    height: categoryHeight,
+                    margin: EdgeInsets.only(right: 5),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: new AssetImage('assets/images/umasara.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                      color: Colors.orange.shade400,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          offset: Offset(2, 2),
+                          blurRadius: 2,
+                          spreadRadius: 0,
+                        )
+                      ],
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            'Umasara Sandamin',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 30.0,
+                              fontFamily: 'AmaticSC',
+                              shadows: <Shadow>[
+                                Shadow(
+                                  color: Colors.black,
+                                  offset: Offset(2, 2),
+                                  blurRadius: 10,
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            'Actor',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.white,
+                              fontFamily: 'Nunito',
+                              shadows: <Shadow>[
+                                Shadow(
+                                  color: Colors.black,
+                                  offset: Offset(1, 1),
+                                  blurRadius: 5,
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () => {},
+                  child: Container(
+                    width: 150,
+                    height: categoryHeight,
+                    margin: EdgeInsets.only(right: 5),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: new AssetImage('assets/images/kavindu.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                      color: Colors.orange.shade400,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          offset: Offset(2, 2),
+                          blurRadius: 2,
+                          spreadRadius: 0,
+                        )
+                      ],
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            'Kavindu Maushanka',
                             style: TextStyle(
                               fontWeight: FontWeight.bold,
                               color: Colors.white,
